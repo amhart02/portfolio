7f0190a4cca63f7bffc22e6f5bacb3f52de0e455
amhart02 <har20010@byui.edu>
2024-10-07 16:07:43 -0600

    Made more changes to the layout of graphic projects and added dynamics as well as a favicon

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    // menu button
     const menuButton = document.querySelector(".menu_button");
 
     function toggleMenu() {
@@ -7,4 +9,34 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     menuButton.addEventListener("click", toggleMenu);
+
+    // img viewer
+    const imgs = document.querySelectorAll(".projectImg");
+    
+    function viewerTemplate(pic, alt) {
+        return `
+        <div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${pic}" alt="${alt}" class="viewer-picture">
+        </div>
+        `;
+    }
+
+    function closeViewer() {
+        const viewer = document.querySelector(".viewer");
+            viewer.remove();
+    }
+
+    function viewHandler(event) {
+        const clickedElement = event.target;
+        const imageSource = clickedElement.src;
+        const newImageAlt = clickedElement.alt;
+        document.body.insertAdjacentHTML("afterbegin", viewerTemplate(imageSource, newImageAlt));
+        const viewerButton = document.querySelector(".close-viewer");
+        viewerButton.addEventListener("click", closeViewer)
+    }
+
+    imgs.forEach(img => {
+        img.addEventListener("click", viewHandler);
+    });
 });
