a07a7abb356083941c05ab077700e9367a20eecd
amhart02 <har20010@byui.edu>
2025-08-24 14:11:43 -0600

    Finishing up writing descriptions

--- src/models/graphicDesign.mjs
+++ src/models/graphicDesign.mjs
@@ -3,7 +3,7 @@ const graphicDesign = [
         id: 1,
         category: "graphicDesign",
         title: "Album Cover",
-        description: "This album cover was created for Haupe by Duke Ellington (1959) as part of an assignment to design a cover for an instrumental song produced before the 1960s. The project specifications allowed for only five colors and required the use of basic shapes (triangle, circle, rectangle, and line), along with the song title, artist's name, and year of release.",
+        description: "This album cover was inspired by and created for Haupe by Duke Ellington (1959) as part of an assignment to design a cover for an instrumental song produced before the 1960s. The project specifications allowed for only five colors and required the use of basic shapes including: a triangle, circle, rectangle, and line. The song title, artist's name, and year of release were also to be incorporated.",
         software:["Adobe Illustrator"],
         date: "May 2024",
         image: "/images/graphicDesign/albumProject.jpg"
@@ -12,7 +12,7 @@ const graphicDesign = [
         id: 2,
         category: "graphicDesign",
         title: "Lepidoptera Insect Replication",
-        description: "For this project, I created a detailed illustration of a butterfly, drawing inspiration from a real Lepidoptera species. Using Adobe Illustrator, I recreated each element to ensure the accuracy of the insect's features, paying close attention to color, texture, and symmetry.",
+        description: "A detailed illustration of a butterfly, drawing inspiration from the real Lepidoptera species. Using Adobe Illustrator, I recreated each element to ensure the accuracy of the insect's features, paying close attention to color, texture, and symmetry.",
         software:["Adobe Illustrator"],
         date: "June 2024",
         image: "/images/graphicDesign/insectDrawing.jpg"
@@ -21,7 +21,7 @@ const graphicDesign = [
         id: 3,
         category: "graphicDesign",
         title: "Brigham Young University - Idaho Campus Map",
-        description: "For this project, I designed a campus map for BYU-Idaho using Adobe InDesign, Illustrator, and Photoshop. The map features detailed guides, photographs of students, and descriptions of key buildings on campus. Overall layout and usability was kept in mind during the development process.",
+        description: "A campus map for BYU-Idaho created using Adobe InDesign, Illustrator, and Photoshop. The map features detailed guides, photographs of students, and descriptions of key buildings on campus. Overall layout and usability was kept in mind during the development process.",
         software:["Adobe Indesign", "Adobe Photoshop"],
         date: "June 2024",
         link: "/images/graphicDesign/byuiCampusMap.pdf",
@@ -31,7 +31,7 @@ const graphicDesign = [
         id: 4,
         category: "graphicDesign",
         title: "Company Logo",
-        description: "For this project, I created a unique logo for a fictional baseball cap company featuring a turtle mascot. The project guidelines required the use of a maximum of three colors and the incorporation of shapes, rather than strokes.",
+        description: "A unique logo for a fictional baseball cap company featuring a turtle mascot. The project guidelines required the use of a maximum of three colors and the incorporation of shapes, rather than strokes.",
         software:["Adobe Illustrator"],
         date: "June 2024",
         image: "/images/graphicDesign/logoProject.jpg"
@@ -40,7 +40,7 @@ const graphicDesign = [
         id: 5,
         category: "graphicDesign",
         title: "Mock Pin",
-        description: "In this project, I designed a pin and packaging centered around the theme 'groovy.' Using Adobe Illustrator to craft the design and Adobe Photoshop to enhance it, I applied shadows and highlights to give the pin a realistic, three-dimensional appearance. The project also involved simulating the product's packaging for a cohesive presentation.",
+        description: "A pin and packaging centered around the theme 'groovy'. Using Adobe Illustrator to craft the design and Adobe Photoshop to enhance it, I applied shadows and highlights to give the pin a realistic, three-dimensional appearance. The project also involved simulating the product's packaging for a cohesive presentation.",
         software:["Adobe Illustrator", "Adobe Photoshop"],
         date: "July 2024",
         image: "/images/graphicDesign/pinProject.jpg"
@@ -49,7 +49,7 @@ const graphicDesign = [
         id: 6,
         category: "graphicDesign",
         title: "Piet Mondrian Art Gallery Pamphlet",
-        description: "I created a mock art gallery booklet for a fictional exhibit featuring the works of Piet Mondrian. This project, designed in Adobe InDesign, Illustrator, and Photoshop, required the inclusion of eight pieces from Mondrian's collection and a description of the artist.",
+        description: "A mock art gallery booklet for a fictional exhibit featuring the works of Piet Mondrian. This project, designed in Adobe InDesign, Illustrator, and Photoshop, required the inclusion of eight pieces from Mondrian's collection and a description of the artist.",
         software:["Adobe InDesign"],
         date: "July 2024",
         link: "/images/graphicDesign/artGalleryBooklet.pdf",
@@ -59,7 +59,7 @@ const graphicDesign = [
         id: 7,
         category: "graphicDesign",
         title: "Pericles Poster",
-        description: "For this assignment, I designed two posters based on Shakespeare's Pericles. The task was to create a symbolic and typographic representation that captured the emotion and meaning of the play. Specific fonts, colors, and symbols attributed to the overall design of the posters.",
+        description: "Two posters based on Shakespeare's Pericles. The task was to create a symbolic and typographic representation that captured the emotion and meaning of the play. An upside down ship was a literal and figurative representation of the life Pericle's once knew coming to an end. The stark contrast of white and dark blue in the type poster indicates a separation of Pericles from himself and from his wife.",
         software:["Adobe Illustrator"],
         date: "July 2024",
         link: "/images/graphicDesign/periclesPoster.pdf",
@@ -69,7 +69,7 @@ const graphicDesign = [
         id: 8,
         category: "graphicDesign",
         title: "Glyph Posters",
-        description: "This project involved creating two cohesive designs using glyphs from two different fonts: Neue Haas Grotesk Display Pro (sans-serif) and Stempel Garamond Lt Std (serif). The goal was to develop a design that visually expressed the unique characteristics and mood of each font style.",
+        description: "Two cohesive designs using glyphs from two different fonts: Neue Haas Grotesk Display Pro (sans-serif) and Stempel Garamond Lt Std (serif). The goal was to develop a design that visually expressed the unique characteristics and mood of each font style.",
         software:["Adobe Illustrator", "Adobe Photoshop"],
         date: "September 2024",
         image: "/images/graphicDesign/glyphProject.jpg"
@@ -78,7 +78,7 @@ const graphicDesign = [
         id: 9,
         category: "graphicDesign",
         title: "Business Card",
-        description: "description",
+        description: "A business card designed for my personal brand. Several variations and colors were explored to find the best representation of my aesthetic. The front and back were photoshopped onto a realistic mockup to show the true size of the cards.",
         software:["Adobe Illustrator", "Adobe Photoshop"],
         date: "October 2024",
         image: "/images/graphicDesign/businessCard.png"
@@ -87,7 +87,7 @@ const graphicDesign = [
         id: 10,
         category: "graphicDesign",
         title: "Baskerville Font Poster",
-        description: "description",
+        description: "A poster design showcasing the Baskerville font. Attention was given to the layout, colors, and size of each element on the poster. The goal was to create a visually appealing representation of the font's characteristics.",
         software:["Adobe Illustrator", "Adobe Photoshop"],
         date: "November 2024",
         image: "/images/graphicDesign/baskervilleFontPoster.jpg"
@@ -96,7 +96,7 @@ const graphicDesign = [
         id: 11,
         category: "graphicDesign",
         title: "Conference Talk Pamphlet",
-        description: "description",
+        description: "A pamphlet designed for a Latter-Day Saint general conference talk called \"Worthiness is not Flawlessness\" by Bradley R. Wilcox. Bold, big lettering and quiet, thin lettering of the quote pages symbolized the all-or-nothing thinking of worthiness as Brad describes in his talk. Cohesive color, font, and hierarchy was involved to create a cohesive and visually interesting design.",
         software:["Adobe Illustrator"],
         date: "December 2024",
         image: "/images/graphicDesign/conferenceTalkPamphlet.png"
