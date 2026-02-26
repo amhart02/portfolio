9ecc3e4968df9a3e8aa3cc9f2503aaedb7ea80d0
Alayna Hart <168308375+amhart02@users.noreply.github.com>
2026-02-26 13:43:05 -0700

    Rearranging projects

--- src/models/graphicDesign.mjs
+++ src/models/graphicDesign.mjs
@@ -2,71 +2,38 @@ const graphicDesign = [
     {
         id: 1,
         category: "graphicDesign",
-        title: "Album Cover",
-        description: "This album cover was inspired by and created for Haupe by Duke Ellington (1959) as part of an assignment to design a cover for an instrumental song produced before the 1960s. The project specifications allowed for only five colors and required the use of basic shapes including: a triangle, circle, rectangle, and line. The song title, artist's name, and year of release were also to be incorporated.",
-        software:["Adobe Illustrator"],
-        date: "May 2024",
-        image: "/images/graphicDesign/albumProject.jpg"
+        title: "PerfectDate Figma File",
+        description: "This figma file contains 3 iterations of prototypes for the website \"Perfect Date\". After each prototype was created, we performed user testing to gather feedback on improvements to be made. Our next iteration will contain new features recommended from our users!",
+        software:["Figma"],
+        date: "January 2026",
+        link: "https://www.figma.com/design/Gxsz05TTNsmMMIPkqUiY3W/PerfectDate-Prototypes?node-id=0-1&t=ahMkoeY6lBEBZahU-1",
+        linkText: "View Figma File",
+        image: "/images/graphicDesign/perfectDate.png"
     },
     {
         id: 2,
-        category: "graphicDesign",
-        title: "Lepidoptera Insect Replication",
-        description: "A detailed illustration of a butterfly, drawing inspiration from the real Lepidoptera species. Using Adobe Illustrator, I recreated each element to ensure the accuracy of the insect's features, paying close attention to color, texture, and symmetry.",
-        software:["Adobe Illustrator"],
-        date: "June 2024",
-        image: "/images/graphicDesign/insectDrawing.jpg"
-    },
-    {
-        id: 3,
-        category: "graphicDesign",
-        title: "Campus Map",
-        description: "A campus map for BYU-Idaho created using Adobe InDesign, Illustrator, and Photoshop. The map features detailed guides, photographs of students, and descriptions of key buildings on campus. Overall layout and usability was kept in mind during the development process.",
-        software:["Adobe Indesign", "Adobe Photoshop"],
-        date: "June 2024",
-        link: "/images/graphicDesign/byuiCampusMap.pdf",
-        image: "/images/graphicDesign/byuiCampusMap.png"
-    },
-    {
-        id: 4,
-        category: "graphicDesign",
-        title: "Company Logo",
-        description: "A unique logo for a fictional baseball cap company featuring a turtle mascot. The project guidelines required the use of a maximum of three colors and the incorporation of shapes, rather than strokes.",
-        software:["Adobe Illustrator"],
-        date: "June 2024",
-        image: "/images/graphicDesign/logoProject.jpg"
-    },
-    {
-        id: 5,
-        category: "graphicDesign",
-        title: "Mock Pin",
-        description: "A pin and packaging centered around the theme 'groovy'. Using Adobe Illustrator to craft the design and Adobe Photoshop to enhance it, I applied shadows and highlights to give the pin a realistic, three-dimensional appearance. The project also involved simulating the product's packaging for a cohesive presentation.",
-        software:["Adobe Illustrator", "Adobe Photoshop"],
-        date: "July 2024",
-        image: "/images/graphicDesign/pinProject.jpg"
-    },
-    {
-        id: 6,
         category: "graphicDesign",
         title: "Art Gallery Pamphlet",
         description: "A mock art gallery booklet for a fictional exhibit featuring the works of Piet Mondrian. This project, designed in Adobe InDesign, Illustrator, and Photoshop, required the inclusion of eight pieces from Mondrian's collection and a description of the artist.",
         software:["Adobe InDesign"],
         date: "July 2024",
         link: "/images/graphicDesign/artGalleryBooklet.pdf",
+        linkText: "View PDF",
         image: "/images/graphicDesign/artGalleryBooklet.png"
     },
     {
-        id: 7,
+        id: 3,
         category: "graphicDesign",
         title: "Pericles Poster",
         description: "Two posters based on Shakespeare's Pericles. The task was to create a symbolic and typographic representation that captured the emotion and meaning of the play. An upside down ship was a literal and figurative representation of the life Pericle's once knew coming to an end. The stark contrast of white and dark blue in the type poster indicates a separation of Pericles from himself and from his wife.",
         software:["Adobe Illustrator"],
         date: "July 2024",
         link: "/images/graphicDesign/periclesPoster.pdf",
+        linkText: "View PDF",
         image: "/images/graphicDesign/periclesPoster.jpg"
     },
     {
-        id: 8,
+        id: 4,
         category: "graphicDesign",
         title: "Glyph Posters",
         description: "Two cohesive designs using glyphs from two different fonts: Neue Haas Grotesk Display Pro (sans-serif) and Stempel Garamond Lt Std (serif). The goal was to develop a design that visually expressed the unique characteristics and mood of each font style.",
@@ -75,7 +42,65 @@ const graphicDesign = [
         image: "/images/graphicDesign/glyphProject.jpg"
     },
     {
+        id: 5,
+        category: "graphicDesign",
+        title: "Conference Talk Pamphlet",
+        description: "A pamphlet designed for a Latter-Day Saint general conference talk called \"Worthiness is not Flawlessness\" by Bradley R. Wilcox. Bold, big lettering and quiet, thin lettering of the quote pages symbolized the all-or-nothing thinking of worthiness as Brad describes in his talk. Cohesive color, font, and hierarchy was involved to create a cohesive and visually interesting design.",
+        software:["Adobe Illustrator"],
+        date: "December 2024",
+        image: "/images/graphicDesign/conferenceTalkPamphlet.png",
+        link: "/images/graphicDesign/conferenceTalkBooklet.pdf",
+        linkText: "View PDF"
+    },
+    {
+        id: 6,
+        category: "graphicDesign",
+        title: "Album Cover",
+        description: "This album cover was inspired by and created for Haupe by Duke Ellington (1959) as part of an assignment to design a cover for an instrumental song produced before the 1960s. The project specifications allowed for only five colors and required the use of basic shapes including: a triangle, circle, rectangle, and line. The song title, artist's name, and year of release were also to be incorporated.",
+        software:["Adobe Illustrator"],
+        date: "May 2024",
+        image: "/images/graphicDesign/albumProject.jpg"
+    },
+    {
+        id: 7,
+        category: "graphicDesign",
+        title: "Lepidoptera Insect Replication",
+        description: "A detailed illustration of a butterfly, drawing inspiration from the real Lepidoptera species. Using Adobe Illustrator, I recreated each element to ensure the accuracy of the insect's features, paying close attention to color, texture, and symmetry.",
+        software:["Adobe Illustrator"],
+        date: "June 2024",
+        image: "/images/graphicDesign/insectDrawing.jpg"
+    },
+    {
+        id: 8,
+        category: "graphicDesign",
+        title: "Campus Map",
+        description: "A campus map for BYU-Idaho created using Adobe InDesign, Illustrator, and Photoshop. The map features detailed guides, photographs of students, and descriptions of key buildings on campus. Overall layout and usability was kept in mind during the development process.",
+        software:["Adobe Indesign", "Adobe Photoshop"],
+        date: "June 2024",
+        link: "/images/graphicDesign/byuiCampusMap.pdf",
+        linkText: "View PDF",
+        image: "/images/graphicDesign/byuiCampusMap.png"
+    },
+    {
         id: 9,
+        category: "graphicDesign",
+        title: "Company Logo",
+        description: "A unique logo for a fictional baseball cap company featuring a turtle mascot. The project guidelines required the use of a maximum of three colors and the incorporation of shapes, rather than strokes.",
+        software:["Adobe Illustrator"],
+        date: "June 2024",
+        image: "/images/graphicDesign/logoProject.jpg"
+    },
+    {
+        id: 10,
+        category: "graphicDesign",
+        title: "Mock Pin",
+        description: "A pin and packaging centered around the theme 'groovy'. Using Adobe Illustrator to craft the design and Adobe Photoshop to enhance it, I applied shadows and highlights to give the pin a realistic, three-dimensional appearance. The project also involved simulating the product's packaging for a cohesive presentation.",
+        software:["Adobe Illustrator", "Adobe Photoshop"],
+        date: "July 2024",
+        image: "/images/graphicDesign/pinProject.jpg"
+    },
+    {
+        id: 11,
         category: "graphicDesign",
         title: "Business Card",
         description: "A business card designed for my personal brand. Several variations and colors were explored to find the best representation of my aesthetic. The front and back were photoshopped onto a realistic mockup to show the true size of the cards.",
@@ -84,22 +109,13 @@ const graphicDesign = [
         image: "/images/graphicDesign/businessCard.png"
     },
     {
-        id: 10,
+        id: 12,
         category: "graphicDesign",
         title: "Baskerville Font Poster",
         description: "A poster design showcasing the Baskerville font. Attention was given to the layout, colors, and size of each element on the poster. The goal was to create a visually appealing representation of the font's characteristics.",
         software:["Adobe Illustrator", "Adobe Photoshop"],
         date: "November 2024",
         image: "/images/graphicDesign/baskervilleFontPoster.jpg"
-    },
-    {
-        id: 11,
-        category: "graphicDesign",
-        title: "Conference Talk Pamphlet",
-        description: "A pamphlet designed for a Latter-Day Saint general conference talk called \"Worthiness is not Flawlessness\" by Bradley R. Wilcox. Bold, big lettering and quiet, thin lettering of the quote pages symbolized the all-or-nothing thinking of worthiness as Brad describes in his talk. Cohesive color, font, and hierarchy was involved to create a cohesive and visually interesting design.",
-        software:["Adobe Illustrator"],
-        date: "December 2024",
-        image: "/images/graphicDesign/conferenceTalkPamphlet.png"
     }
 ]
 
