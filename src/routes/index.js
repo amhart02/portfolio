91f86d9665153bd7144936f2d4b6e4fee099fc13
Alayna Hart <168308375+amhart02@users.noreply.github.com>
2026-02-26 14:14:23 -0700

    Adding current project to index page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import websites from '../models/websites.mjs';
+import graphicDesign from '../models/graphicDesign.mjs';
 
 const router = Router();
 
@@ -6,8 +8,13 @@ router.get('/', (req, res) => {
     const title = 'Alayna Hart | Portfolio';
     const h1 = "Alayna Hart";
     const h2 = "Hi I'm";
-    const h3 = "Web Designer and Developer"
-    res.render('index', { title, h1, h2, h3 });
+    const h3 = "Web Designer and Developer";
+    
+    // Get PerfectDate project from both arrays
+    const perfectDateWebsite = websites.find(p => p.title === "PerfectDate");
+    const perfectDateFigma = graphicDesign.find(p => p.title === "PerfectDate Figma File");
+    
+    res.render('index', { title, h1, h2, h3, perfectDateWebsite, perfectDateFigma });
 })
 
 export default router;
